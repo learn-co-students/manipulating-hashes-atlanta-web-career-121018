3dfb67db7d2c7692d47b7e62d1d04b072c50a0c5
github username <github email address>
2018-12-24 03:12:20 -0500

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,14 @@
-def second_challenge
-  groceries = {
-   dairy: ["milk", "yogurt", "cheese"],
-   vegetable: ["carrots", "broccoli", "cucumbers"],
-   meat: ["chicken", "steak", "salmon"],
-   grains: ["rice", "pasta"]
-  }
+require'pry'
 
-  groceries.values.flatten
-  
+
+def second_challenge
+    groceries = {
+        dairy: ["milk", "yogurt", "cheese"],
+        vegetable: ["carrots", "broccoli", "cucumbers"],
+        meat: ["chicken", "steak", "salmon"],
+        grains: ["rice", "pasta"]
+    }
+
+    groceries.values.flatten
 
 end
